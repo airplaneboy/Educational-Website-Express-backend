2131358dcdf6e39f223680643272bccbacd6280d
Agara Sulaiman <agarasualimany@gmail.com>
2023-03-25 03:49:42 +0100

    Updated user schema

--- models/users.js
+++ models/users.js
@@ -64,6 +64,12 @@ const userSchema = mongoose.Schema(
     },
 
     //Additional
+    progress: {
+      completedLessons: [
+        { type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' },
+      ],
+      currentLesson: { type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' },
+    },
     enrolledCourses: {
       type: [Schema.Types.ObjectId],
       ref: 'Courses',
@@ -76,15 +82,38 @@ const userSchema = mongoose.Schema(
       type: [Schema.Type.ObjectId],
       ref: 'Courses',
     },
-    comments: {
-      type: [String],
-      timestamps: true,
-    },
-    notifications: {
-      type: [Notifications],
-    },
+    comments: [
+      {
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+        lessonId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Lesson',
+          required: true,
+        },
+        // Add any other relevant fields like author, replies, etc.
+      },
+    ],
+    notifications: [
+      {
+        type: { type: String, required: true },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+        lessonId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Lesson',
+          required: true,
+        },
+        // other relevant fields like sender, link, etc.
+      },
+    ],
     settings: {
-      type: [Settings],
+      language: { type: String, default: 'en' },
+      emailNotifications: { type: Boolean, default: true },
+      username: { type: String, required: true },
+      hashedPassword: { type: String, required: true },
+      profilePicture: { type: String },
+      bio: { type: String },
     },
   },
   { timestamps: true }
